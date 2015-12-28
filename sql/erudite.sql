1f49baa9606560e2b8d84269a3fd4af6366dbc57
cloud9-debasish <debasishme7@gmail.com>
2015-12-28 13:24:59 +0530

    updated database
    sql

--- sql/erudite.sql
+++ sql/erudite.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 16, 2015 at 08:54 PM
--- Server version: 5.1.61-community
+-- Generation Time: Dec 28, 2015 at 08:52 AM
+-- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -19,6 +19,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `erudite`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` int(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `area`
+--
+
+CREATE TABLE IF NOT EXISTS `area` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -40,6 +64,118 @@ CREATE TABLE IF NOT EXISTS `executive_table` (
 
 INSERT INTO `executive_table` (`ex_id`, `ex_user_id`, `ex_password`, `ex_name`) VALUES
 (1, 'executive_user1', 'secure', 'user1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `segment`
+--
+
+CREATE TABLE IF NOT EXISTS `segment` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `segment`
+--
+
+INSERT INTO `segment` (`id`, `name`) VALUES
+(1, 'class i-vi'),
+(2, 'class vii-x'),
+(3, 'class xi-xii');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `segmentsubject`
+--
+
+CREATE TABLE IF NOT EXISTS `segmentsubject` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `segmentId` int(10) NOT NULL,
+  `subjectId` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `segmentId` (`segmentId`),
+  KEY `subjectId` (`subjectId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `segmentsubject`
+--
+
+INSERT INTO `segmentsubject` (`id`, `segmentId`, `subjectId`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 3),
+(4, 2, 2),
+(5, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE IF NOT EXISTS `subject` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`id`, `name`) VALUES
+(1, 'English'),
+(2, 'Math'),
+(3, 'Physics');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacherarea`
+--
+
+CREATE TABLE IF NOT EXISTS `teacherarea` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `teacherId` int(10) NOT NULL,
+  `areaId` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `teacherId` (`teacherId`,`areaId`),
+  KEY `areaId` (`areaId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachersegmentsubject`
+--
+
+CREATE TABLE IF NOT EXISTS `teachersegmentsubject` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `teacherId` int(10) NOT NULL,
+  `segmentId` int(10) NOT NULL,
+  `subjectId` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `teacherId` (`teacherId`,`segmentId`,`subjectId`),
+  KEY `segmentId` (`segmentId`),
+  KEY `subjectId` (`subjectId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -81,6 +217,32 @@ CREATE TABLE IF NOT EXISTS `teacher_details` (
   `t_doneby` varchar(50) NOT NULL,
   PRIMARY KEY (`t_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `segmentsubject`
+--
+ALTER TABLE `segmentsubject`
+  ADD CONSTRAINT `segmentsubject_ibfk_2` FOREIGN KEY (`subjectId`) REFERENCES `subject` (`id`),
+  ADD CONSTRAINT `segmentsubject_ibfk_1` FOREIGN KEY (`segmentId`) REFERENCES `segment` (`id`);
+
+--
+-- Constraints for table `teacherarea`
+--
+ALTER TABLE `teacherarea`
+  ADD CONSTRAINT `teacherarea_ibfk_2` FOREIGN KEY (`areaId`) REFERENCES `area` (`id`),
+  ADD CONSTRAINT `teacherarea_ibfk_1` FOREIGN KEY (`teacherId`) REFERENCES `teacher_details` (`t_id`);
+
+--
+-- Constraints for table `teachersegmentsubject`
+--
+ALTER TABLE `teachersegmentsubject`
+  ADD CONSTRAINT `teachersegmentsubject_ibfk_3` FOREIGN KEY (`subjectId`) REFERENCES `subject` (`id`),
+  ADD CONSTRAINT `teachersegmentsubject_ibfk_1` FOREIGN KEY (`teacherId`) REFERENCES `teacher_details` (`t_id`),
+  ADD CONSTRAINT `teachersegmentsubject_ibfk_2` FOREIGN KEY (`segmentId`) REFERENCES `segment` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
