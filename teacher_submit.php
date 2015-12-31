dc37fcdb7211b5f4fb0987b5d0361a1161803731
Cloud9-Debasish <debasishme7@gmail.com>
2015-12-31 11:52:51 +0530

    done

--- teacher_submit.php
+++ teacher_submit.php
@@ -1,16 +1,30 @@
+
 <?php
-error_reporting(E_ALL ^ E_DEPRECATED);
-$link=mysql_connect("localhost","root","")or die("can not connect");
-		mysql_select_db("erudite",$link) or die("can not select database");
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "erudite";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password,$dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+	
 	$chk=$_POST['chk'];
+
+
 	
-	
-		for ($i=0; $i<sizeof(chk);$i++){
+	for ($i=0; $i<sizeof($chk);$i++){
 		 	$query= "INSERT INTO admin (name) values('".$chk[$i]."')";
-			mysql_query($query,$link)or die(mysql_error());
+			$conn->query($query);
 			}
-		mysql_close($link);
-		echo "<script>alert('Data stored successfully!')</script>";
-		
+echo "<script>alert('Data stored successfully!')</script>";
+	$conn->close();
+	
+	
+	
 
 ?>
